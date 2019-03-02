949d55c9c4cb96a3ecf9b8855eb4b750ead002f9
Riccardo Dalla Via <rikydallavia1997@gmail.com>
2019-03-03 00:20:31 +0100

    Test

--- src/component/pages/add_network/add_network.js
+++ src/component/pages/add_network/add_network.js
@@ -19,14 +19,14 @@ class AddNetworkCtrl {
 
         r.onload = e => {
             let contents = e.target.result;
-            let network = Network.fromJSON(contents);
+            //let network = Network.fromJSON(contents);
             let database = document.getElementById("databases");
             let databaseID = this.databases.find(c => c.name === database.options[database.selectedIndex].text);
 
-            network.DBWriteName = databaseID.database;
+            /*network.DBWriteName = databaseID.database;
             network.DBWriteUrl = databaseID.url;
             network.DBWriteUser = databaseID.user;
-            network.DBWritePassword = databaseID.password;
+            network.DBWritePassword = databaseID.password;*/
 
             //saveNetwork(this.$http, network.toJSON());
             console.log(contents);
